48cebdb8c7ff2b6b7efaa9586d8553be278dba72
DESKTOP-FRTFPVS\User <k.larionov.02@mail.ru>
2021-12-09 23:07:48 +0300

    Start creating search icons

--- componets/search/search.js
+++ componets/search/search.js
@@ -1,26 +1,68 @@
-import React from "react";
-import { StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
-const Search = (searchData) =>{
-    return (
-        <TextInput
-            style = {styles.search}/>
-    )
-}
+const Search = (searchData) => {
+  const [request, setRequest] = useState();
+
+  return (
+    <View>
+      <View style={styles.wrapper}>
+        <View style = {styles.flexGroup}>
+          <Image
+            style={[styles.searchIcon, styles.icon]}
+            source={require("../../assets/search/search.png")}
+          />
+          <TouchableWithoutFeedback>
+            <Image
+              source={require("../../assets/search/delete.png")}
+              style={[styles.deleteIcon, styles.icon]}
+            />
+          </TouchableWithoutFeedback>
+        </View>
+        <TextInput style={styles.search} placeholder="Ищите что-то" />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    search:{
-        backgroundColor: '#F7F7F7',
-        borderRadius: 11,
-        borderWidth: 2,
-        borderColor: '#363636',
-        marginTop: 11,
-        marginHorizontal: 9
+  wrapper: {
+    position: "relative",
+    marginTop: 11,
+    marginHorizontal: 9,
+  },
+  search: {
+    backgroundColor: "#F7F7F7",
+    borderRadius: 11,
+    borderWidth: 2,
+    borderColor: "#363636",
 
-    },
-    container:{
-        borderWidth: 0
-    }
-})
+    paddingLeft: 42,
+  },
+  container: {
+    borderWidth: 0,
+  },
+  icon: {
+    position: "absolute",
+    zIndex: 10,
+  },
+  searchIcon: {
+    zIndex: 10,
+  },
+  deleteIcon: {
+    right: 0,
+  },
+  flexGroup:{
+      alignItems: 'flex-start',
+      justifyContent: 'space-between',
+      flexDirection:'row'
+  }
+});
 
 export default Search;
